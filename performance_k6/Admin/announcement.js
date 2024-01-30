f242911f437fe31d542b05dbc3f4eca3b492202c
qasalmangrib <msalman.magribie@gmail.com>
2024-01-30 23:15:27 +0700

    rename

---
+++ performance_k6/Admin/announcement.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { sleep,check } from 'k6';
+
+export const options = {
+    stages: [
+        { duration: '30s', target:100 }, // ramp up
+        { duration: '30s', target:100 }, // stable
+        { duration: '30s', target:0 }, // ramp down to 0
+    ],
+};
+
+export default () => {
+    const res =  http.get('https://edu-cat.com.my/announcement')
+    check(res, {"response code was 200": (res) => res.status == 200,});    
+    sleep(3);
+};
